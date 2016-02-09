27d6a2998536e98017fde5b74afbea2a6b50d69f
Andreas Larsen <northcode.no@gmail.com>
2016-02-09 22:19:55 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BASEDIR=$(readlink -f $(dirname $0))
+
+echo "linking config folders"
+
+for i in $(ls -a .config)
+do
+    if test $i != "." && test $i != ".."
+    then
+	rm ~/.config/$i
+	ln -sf $BASEDIR/.config/$i ~/.config/$i
+    fi
+done
+
+echo "linking emacs config"
+rm ~/.emacs
+ln -sf $BASEDIR/.emacs ~/.emacs
+
+echo "Removing legacy ncmpcpp config"
+rm -r ~/.ncmpcpp/
+
+echo "Linking user bin folder"
+rm ~/bin
+ln -sf $BASEDIR/bin ~/bin
+
+echo "linking xinitrc"
+rm ~/.xinitrc
+ln -sf ~/.config/X/xinitrc ~/.xinitrc 
+
+echo "linking zshrc"
+rm ~/.zshrc
+ln -sf $BASEDIR/.zshrc ~/.zshrc
