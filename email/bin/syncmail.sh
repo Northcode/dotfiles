9933f82a5a1668caa8db8ce83cc460fab68ab5a8
Andreas Larsen <andreas@northcode.no>
2018-05-22 12:40:32 +0200

    Add notification to syncmail

--- email/bin/syncmail.sh
+++ email/bin/syncmail.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 mbsync -aV
 mu index -m ~/.mail
+
+UNREADCOUNT=$(mu find 'flag:unread AND NOT maildir:/northcode/Junk' | wc -l)
+
+if [ $UNREADCOUNT -gt 0 ]; then
+    notify-send "New mail!" "$UNREADCOUNT new emails synced!"
+fi
