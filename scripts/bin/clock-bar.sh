fa0e774d85a8331fa144d0cdccece2d36711f03c
Andreas Larsen <andreas@northcode.no>
2018-09-17 10:09:44 +0200

    Updated bar script

--- scripts/bin/clock-bar.sh
+++ scripts/bin/clock-bar.sh
@@ -5,6 +5,12 @@ BG='#CC111111'
 # FONT='-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*'
 FONT='xft:Hack:pixelsize=14:antialias=true'
 
+BAR_X=0
+if [ "$(hostname)" == "andreas-d0" ]
+then
+    BAR_X=1920 # custom offset for 3 screens
+fi
+
 while true
 do
     clock=$(date +"%H:%M")
@@ -16,4 +22,4 @@ do
     
     echo '%{r}' "vpn: $vpn_active | unread email: $unread_mails | $clock | $date  "
     sleep 10
-done | lemonbar -g 1920x25+1920+1055 -f $FONT -B $BG -F $FG
+done | lemonbar -g 1920x25+$BAR_X+1055 -f $FONT -B $BG -F $FG
