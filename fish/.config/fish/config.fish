11494038a61c489dc58b681c6166eb8fdbc5ba67
Andreas Larsen <andreas@northcode.no>
2020-04-14 20:24:19 +0200

    fix config.fish error if $XDG_VTNR or $XDG_SESSION_TYPE is not set

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-if test $XDG_VTNR = 1 -a $XDG_SESSION_TYPE = tty -a -z "$SWAYSOCK"
+if test "$XDG_VTNR" = '1' -a "$XDG_SESSION_TYPE" = "tty" -a -z "$SWAYSOCK"
 	set PATH $HOME/bin $PATH
 	set MOZ_ENABLE_WAYLAND 1
 	export PATH
