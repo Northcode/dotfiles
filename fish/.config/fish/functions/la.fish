4ebad4f54fdcb86b7b3cd9cb7bef12d7763e4643
Andreas Larsen <andreas@northcode.no>
2024-08-13 00:53:29 +0200

    fish: alias ls,ll,la to exa

---
+++ fish/.config/fish/functions/la.fish
@@ -0,0 +1,4 @@
+function la --wraps='exa -la' --description 'alias la exa -la'
+  exa -la $argv
+        
+end
---
+++ fish/.config/fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps='exa -l' --wraps='exa -la' --description 'alias ll exa -l'
+  exa -l $argv
+        
+end
